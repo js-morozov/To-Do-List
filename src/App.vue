617e3243ee86dd653ce4915d62347c08e0d00e5d
js_morozov <frontend.morozov@gmail.com>
2020-08-13 22:10:15 +0300

    Sidebar & Contentbar headers

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main class="app-container">
+      <aside class="sidebar">
+        <div class="sidebar__header">
+          <a href="#" class="sidebar__header-logo">
+            <img src="@/assets/images/logo.svg" alt="To do list" />
+            <span>To Do Manager</span>
+          </a>
+        </div>
+      </aside>
+      <section class="contentbar">
+        <div class="contentbar__header">
+          <h1 class="pagetitle">All Tasks</h1>
+          <input type="text" class="search" placeholder="Search..." />
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "reset-css";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+body {
+  background: #f9f9fb;
+  padding: 24px;
+}
+.app-container {
+  display: flex;
+}
+.sidebar {
+  width: 300px;
+  background: #ffffff;
+  box-shadow: 0px 1px 4px rgba(205, 209, 212, 0.64);
+  border-radius: 8px;
+  margin-right: 32px;
+  &__header {
+    display: flex;
+    align-items: center;
+    height: 80px;
+    padding-left: 24px;
+    &-logo {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+      span {
+        font-size: 16px;
+        line-height: 1.4;
+        color: #120e21;
+        font-weight: bold;
+        padding: 0 12px;
+      }
+    }
+  }
+}
+.contentbar {
+  flex-grow: 1;
+  background: #ffffff;
+  box-shadow: 0px 1px 4px rgba(205, 209, 212, 0.64);
+  border-radius: 8px;
+  padding: 40px 48px;
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+.pagetitle {
+  font-size: 32px;
+  line-height: 1.2;
+  color: #120e21;
+  font-weight: bold;
+}
+.search {
+  width: 325px;
+  height: 40px;
+  padding: 0 32px 0 48px;
+  background: #ffffff;
+  border: 1px solid #d6d6d6;
+  border-radius: 24px;
+  font-size: 14px;
+  font-weight: 500;
+  background: #ffffff url(./assets/images/search.svg) 16px center no-repeat;
+  &:focus {
+    outline: none;
+  }
 }
 </style>
---
+++ src/components/controls/BaseButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="button">Add Task</button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {}
+};
+</script>
+
+<style lang="scss">
+.button {
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 240px;
+  height: 40px;
+  border-radius: 20px;
+  background: #9378ff;
+  box-shadow: 0px 3px 2px rgba(147, 120, 255, 0.02),
+    0px 7px 5px rgba(147, 120, 255, 0.03),
+    0px 12px 10px rgba(147, 120, 255, 0.035),
+    0px 22px 18px rgba(147, 120, 255, 0.04),
+    0px 42px 33px rgba(147, 120, 255, 0.05),
+    0px 100px 80px rgba(147, 120, 255, 0.07);
+  font-size: 14px;
+  font-weight: 500;
+  text-align: center;
+  color: #ffffff;
+  border: none;
+  outline: none;
+  user-select: none;
+}
+</style>
